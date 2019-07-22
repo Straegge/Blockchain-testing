e1ee3e72d148ffa042fd4b5624dfd19337c275f6
Straegge <straeggeVins@protonmail.com>
2019-07-22 16:46:06 +0200

    Add CreateProjectButton and Project entity

--- src/main/java/com/the_pangaea_paradigm/ui/components/projectpage/ProjectActionContainer.java
+++ src/main/java/com/the_pangaea_paradigm/ui/components/projectpage/ProjectActionContainer.java
@@ -1,16 +1,31 @@
 package com.the_pangaea_paradigm.ui.components.projectpage;
 
+import com.the_pangaea_paradigm.ui.components.global.TPPButton;
 import com.the_pangaea_paradigm.utilities.StyledComponent;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 public class ProjectActionContainer extends HorizontalLayout implements StyledComponent {
 
     public ProjectActionContainer() {
         style();
+
+        addCreateProjectButton();
+    }
+
+    private void addCreateProjectButton() {
+        //TODO: Add ComponentEventListener to open ProjectCreationView
+        Component createProjectButton = new TPPButton("", ClickEvent::getClickCount);
+        add(createProjectButton);
     }
 
     @Override
     public void style() {
-
+        setHeight("100px");
+        setWidthFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        getStyle()
+                .set("background-color", "white");
     }
 }
---
+++ src/main/java/com/the_pangaea_paradigm/utilities/Project.java
@@ -0,0 +1,19 @@
+package com.the_pangaea_paradigm.utilities;
+
+/**
+ * The entity class for a Project.
+ * Used for adding projects to and rendering projects from IFPS.
+ */
+public final class Project {
+    private String name;
+    private String description;
+
+    public Project() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
