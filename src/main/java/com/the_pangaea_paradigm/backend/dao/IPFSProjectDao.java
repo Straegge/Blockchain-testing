0c031a1a1745dea1796cec0ae8a2717ebccbbcd0
Straegge <straeggeVins@protonmail.com>
2019-07-24 13:12:40 +0200

    Update JavaDoc of DAOs

--- src/main/java/com/the_pangaea_paradigm/backend/dao/IPFSProjectDao.java
+++ src/main/java/com/the_pangaea_paradigm/backend/dao/IPFSProjectDao.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Concrete Data Access Object for executing CRUD operations on Projects
+ * in communication with IPFS as data persistence layer.
+ */
 public class IPFSProjectDao implements ProjectDao {
 
     @Override
--- src/main/java/com/the_pangaea_paradigm/backend/dao/ProjectDao.java
+++ src/main/java/com/the_pangaea_paradigm/backend/dao/ProjectDao.java
@@ -5,13 +5,16 @@ import com.the_pangaea_paradigm.backend.dataobjects.Project;
 import java.util.List;
 import java.util.Optional;
 
-//TODO: Implement Abstract Factory Pattern for different Data Access Object strategies
+//TODO: Implement Abstract Factory Pattern for different data persistence strategies
 
 /**
- * Data Access Object Interface for Projects.
- * <p>
- * Keeps the Project model decoupled from the persistence layer, and as such all data could be
- * fetched/stored from/to IPFS or any other storage mechanism.
+ * Data Access Object Interface for executing CRUD operations on Projects.
+ *
+ * Single Responsibility Principle is generally thought to be achieved by object-specific
+ * DAO classes since their only responsibility is persistence of that data object.
+ *
+ * Keeps the Project model decoupled from the persistence layer, and as such all data
+ * could be fetched/stored from/to IPFS or any other storage mechanism.
  */
 public interface ProjectDao {
 
