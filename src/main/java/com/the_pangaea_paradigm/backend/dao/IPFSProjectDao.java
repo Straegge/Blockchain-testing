6b2ca76b8e55374b0de14fdf6ad35fbf1313a1bb
Straegge <straeggeVins@protonmail.com>
2019-07-27 17:02:10 +0200

    Move Infura IPFS Gateway instance to GlobalConstants

--- src/main/java/com/the_pangaea_paradigm/backend/dao/IPFSProjectDao.java
+++ src/main/java/com/the_pangaea_paradigm/backend/dao/IPFSProjectDao.java
@@ -6,7 +6,6 @@ import com.the_pangaea_paradigm.Application;
 import com.the_pangaea_paradigm.backend.dataobjects.Project;
 import com.the_pangaea_paradigm.backend.dataobjects.ProjectList;
 import com.the_pangaea_paradigm.utilities.GlobalConstants;
-import io.ipfs.api.IPFS;
 import io.ipfs.api.MerkleNode;
 import io.ipfs.api.NamedStreamable;
 import io.ipfs.multihash.Multihash;
@@ -22,8 +21,6 @@ import java.util.Optional;
 @Repository
 public class IPFSProjectDao implements ProjectDao {
 
-    private static final IPFS ipfs = new IPFS(GlobalConstants.INFURA_IPFS_GATEWAY_MULTIADDRESS);
-
     @Override
     public Optional<Project> get(long id) {
         return Optional.empty();
@@ -32,9 +29,9 @@ public class IPFSProjectDao implements ProjectDao {
     @Override
     public ProjectList getAll() {
 
-        System.out.println("IPFS Protocol: " + ipfs.protocol);
-        System.out.println("IPFS Host: " + ipfs.host);
-        System.out.println("IPFS Port: " + ipfs.port + "\n");
+        System.out.println("IPFS Protocol: " + GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.protocol);
+        System.out.println("IPFS Host: " + GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.host);
+        System.out.println("IPFS Port: " + GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.port + "\n");
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         Project projectToAdd = new Project("Best Project Name 5", "Best Project Description 5");
@@ -47,8 +44,8 @@ public class IPFSProjectDao implements ProjectDao {
             //Fetch ProjectList
             directoryMultihash = Application.PROJECT_LIST_FILE_IPFS_HASH;
 
-            System.out.println(new String(ipfs.cat(directoryMultihash)));
-            ProjectList projectListMerkleDagFromDag = gson.fromJson(new String(ipfs.cat(directoryMultihash)), ProjectList.class);
+            System.out.println(new String(GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.cat(directoryMultihash)));
+            ProjectList projectListMerkleDagFromDag = gson.fromJson(new String(GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.cat(directoryMultihash)), ProjectList.class);
 
             //Add Project
             projectListMerkleDagFromDag.getProjects().add(projectToAdd);
@@ -60,14 +57,14 @@ public class IPFSProjectDao implements ProjectDao {
                     new NamedStreamable.ByteArrayWrapper(projectListMerkleDagString.getBytes());
 
             //Add new ProjectList
-            addResult = ipfs.add(byteArrayWrapper, true, false).get(0);
+            addResult = GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.add(byteArrayWrapper, true, false).get(0);
             System.out.println("Add Result: " + addResult.toJSONString() + "\n");
 
             //Fetch ProjectList again
             newDirectoryMultihash = addResult.hash;
 
-            System.out.println(new String(ipfs.cat(newDirectoryMultihash)));
-            newProjectListMerkleDagFromDag = gson.fromJson(new String(ipfs.cat(newDirectoryMultihash)), ProjectList.class);
+            System.out.println(new String(GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.cat(newDirectoryMultihash)));
+            newProjectListMerkleDagFromDag = gson.fromJson(new String(GlobalConstants.INFURA_IPFS_GATEWAY_INSTANCE.cat(newDirectoryMultihash)), ProjectList.class);
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/the_pangaea_paradigm/utilities/GlobalConstants.java
+++ src/main/java/com/the_pangaea_paradigm/utilities/GlobalConstants.java
@@ -1,5 +1,7 @@
 package com.the_pangaea_paradigm.utilities;
 
+import io.ipfs.api.IPFS;
+
 /**
  * Stores global constants used in the application. The constants are not interchangeable,
  * so this isn't the right use case for an Enum.
@@ -8,5 +10,5 @@ public class GlobalConstants {
     public static final String LOGO = "frontend/images/Logo.png";
     public static final String LOGO_WITH_NAME = "frontend/images/LogoWithName.png";
     public static final String GITHUB_ADDRESS = "https://github.com/Straegge/the-pangaea-paradigm";
-    public static final String INFURA_IPFS_GATEWAY_MULTIADDRESS = "/dnsaddr/ipfs.infura.io/tcp/5001/https";
+    public static final IPFS INFURA_IPFS_GATEWAY_INSTANCE = new IPFS("/dnsaddr/ipfs.infura.io/tcp/5001/https");
 }
