25c4729dd986ca9b0fa4637cee3f3001c88c4965
Straegge <straeggeVins@protonmail.com>
2019-07-29 21:29:17 +0200

    Add TextFields to ProjectApplicationForm

--- src/main/java/com/the_pangaea_paradigm/ui/components/projectpage/ProjectApplicationForm.java
+++ src/main/java/com/the_pangaea_paradigm/ui/components/projectpage/ProjectApplicationForm.java
@@ -5,16 +5,30 @@ import com.the_pangaea_paradigm.backend.dataobjects.Project;
 import com.the_pangaea_paradigm.utilities.StyledComponent;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProjectApplicationForm extends Composite<FormLayout> implements StyledComponent {
+
+    private TextField description = new TextField();
+    private List<TextField> requiredSkillSets = new ArrayList<>();
+    private TextField emailAddress = new TextField();
+    private List<TextField> otherContactDetails = new ArrayList<>();
+    private Binder<Applicant> binder = new Binder<>(Applicant.class);
 
     public ProjectApplicationForm(Project project) {
         style();
         FormLayout parent = getContent();
 
-        BeanValidationBinder<Applicant> binder = new BeanValidationBinder<>(Applicant.class);
+        parent.addFormItem(createNameField(), "Name:");
+    }
 
+    private TextField createNameField() {
+        TextField name = new TextField();
+        return name;
     }
 
     @Override
