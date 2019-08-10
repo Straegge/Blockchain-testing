975fb8cfd5fd7b6578ee955d9cd19b0bdbc08d78
Straegge <straeggeVins@protonmail.com>
2019-08-10 19:53:21 +0200

    Fix regex for E-Mail addresses

--- src/main/java/com/the_pangaea_paradigm/backend/dto/Applicant.java
+++ src/main/java/com/the_pangaea_paradigm/backend/dto/Applicant.java
@@ -2,6 +2,7 @@ package com.the_pangaea_paradigm.backend.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
@@ -21,8 +22,12 @@ public class Applicant implements Serializable {
     @Size(max = 200, message = "Please list your skill sets in less than 200 characters")
     private String skillSets;
 
-    @Email
-    @NotBlank(message = "Please enter your E-Mail address")
+    @Pattern(regexp = "(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x" +
+            "0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[" +
+            "a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\" +
+            ".){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x2" +
+            "1-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])",
+            message = "Please enter a valid E-Mail Address")
     private String emailAddress;
 
     @Size(max = 200, message = "Please list your other contact details in less than 200 characters")
--- src/main/java/com/the_pangaea_paradigm/backend/dto/Project.java
+++ src/main/java/com/the_pangaea_paradigm/backend/dto/Project.java
@@ -2,6 +2,7 @@ package com.the_pangaea_paradigm.backend.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
@@ -26,15 +27,17 @@ public class Project implements Serializable {
     @Size(max = 1500, message = "Description must be less than or equal to 1500 characters")
     private String longDescription;
 
-    @Size(max = 200, message = "Please list some skill sets needed for your Project in less than 200 characters")
+    @Size(max = 200, message = "Please list some Skill Sets needed for your Project in less than 200 characters")
     private String requiredSkillSets;
 
-    @Email
-    @NotBlank(message = "Please enter the E-Mail address you want to receive applications to")
+    @Pattern(regexp = "(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x" +
+            "0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[" +
+            "a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\" +
+            ".){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x2" +
+            "1-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])",
+            message = "Please enter a valid E-Mail Address")
     private String emailAddress;
 
-    @NotBlank(message = "Please enter the Ethereum address of your Project")
-    @Size(max = 100, message = "Ethereum address must be less than or equal to 100 characters")
     private String ethereumAddress;
 
     public Project() {
